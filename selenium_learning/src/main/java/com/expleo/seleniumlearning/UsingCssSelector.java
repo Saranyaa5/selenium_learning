9eff85c55db536b3765924823b2edfbb8493f920
Saranyaa5 <2k21cse128@kiot.ac.in>
2025-03-17 23:51:20 +0530

    ADDED cssselectorfile

---
+++ selenium_learning/src/main/java/com/expleo/seleniumlearning/UsingCssSelector.java
@@ -0,0 +1,38 @@
+package com.expleo.seleniumlearning;
+
+import java.time.Duration;
+
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.edge.EdgeDriver;
+
+public class UsingCssSelector {
+	public static void main(String [] args) {
+		WebDriver driver=new EdgeDriver();
+		 driver.manage().window().maximize();
+		driver.manage().timeouts().implicitlyWait(Duration.ofSeconds(10));
+		driver.get("https://www.naukri.com");
+		try {
+			WebElement searchBox=driver.findElement(By.cssSelector("input.suggestor-input"));
+			System.out.println("search box");
+			System.out.println("displayed: "+searchBox.isDisplayed());
+			System.out.println("Enabled: "+searchBox.isEnabled());
+		}
+		catch(Exception e){
+			System.out.println("Search box not found");
+		}
+		try {
+			WebElement searchButton=driver.findElement(By.cssSelector("div.qsbSubmit"));
+			System.out.println("search box");
+			System.out.println("displayed: "+searchButton.isDisplayed());
+			System.out.println("Enabled: "+searchButton.isEnabled());
+		}
+		catch(Exception e){
+			System.out.println("Search box not found");
+			
+		}
+		driver.quit();
+	}
+}
+
