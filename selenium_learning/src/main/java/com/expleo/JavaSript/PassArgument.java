62223aa05ec65692e6709e3dd30dc9ff5a0c0539
Saranyaa5 <2k21cse128@kiot.ac.in>
2025-03-18 21:14:12 +0530

    Cleaned target file

--- selenium_learning/src/main/java/com/expleo/JavaSript/PassArgument.java
+++ selenium_learning/src/main/java/com/expleo/JavaSript/PassArgument.java
@@ -1,6 +1,7 @@
 package com.expleo.JavaSript;
 
 import org.openqa.selenium.By;
+
 import org.openqa.selenium.JavascriptExecutor;
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.WebElement;
